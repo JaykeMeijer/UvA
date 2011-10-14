764cc1870552477ae6406ef1aaed9911a8d34454
Richard Torenvliet <rich.riz@gmail.com>
2011-10-14 14:01:13 +0200

    Gauss functie werkt

--- Beeldbewerken/Exercise 4/gauss.py
+++ Beeldbewerken/Exercise 4/gauss.py
@@ -5,22 +5,23 @@ from numpy import zeros
 
 def f(s, n, x, y):
    """Gaussian function"""
-   return 1 / (2 * pi * (s ** 2)) * (e ** -((x - n / 2)**2 + (y - n / 2)**2 ) / (2 * (s ** 2)))
+   return 1 / (2 * pi * (s ** 2)) * (e ** -(((x - n / 2)**2 + (y - n / 2)**2 ) 
+                                                              / (2 * (s ** 2))))
         
 # Gaussian filter with a kernel of 6s - 1 values
 def gauss(s):
     """Two dimensional Gaussian mask"""
-    # one for verbose, zero for non-verbose
+    # 1 for verbose, 0 for non-verbose
     debug = 0;    
     
     # if s is even, the heighest value will not be in the middle of the 2D
-    # array
+    # array.
     # a = 1 for even and 0 for odd
-    a = 1 if (int(s) % 2 == 0) else 0
+    #a = 1 if (int(s) % 2 == 0) else 0
         
     # for sufficient result use ceil(6s) by ceil(6s) for a gaussian filter
     # read: http://en.wikipedia.org/wiki/Gaussian_blur for more explaination     
-    n = int(ceil(6 * s) - a)
+    n = int(ceil(6 * s) + 1)
     s = float(s)
     
     # n * n zero matrix of floats3
@@ -33,12 +34,18 @@ def gauss(s):
             if debug: print("%.4f" % gaussFilter[i][j]),
         if debug: print '\n'
     
+    return gaussFilter / gaussFilter.sum()
+    
 F = imread('cameraman.png') 
 
-G = convolve(F, gauss(2), mode='nearest')
+B = gauss(3)
+
+imshow(B, cmap='gray')
+show()
+#G = convolve(F, B, mode='nearest')
 
 subplot(111)
 imshow(F, cmap ='gray')
-show()
+
 subplot(122)
 imshow(G, cmap ='gray') 
