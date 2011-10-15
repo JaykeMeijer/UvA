e7bea5df9221ed579dd4ccbf18aa2e2af439c164
Richard Torenvliet <rich.riz@gmail.com>
2011-10-15 09:14:39 +0200

    Gauss function working, python took the wrong numpy.convolve function instead of scipy convolve

--- Beeldbewerken/Exercise 4/gauss.py
+++ Beeldbewerken/Exercise 4/gauss.py
@@ -1,8 +1,10 @@
 # By Jayke and Richard
 from scipy.ndimage import convolve
-from pylab import *
+from pylab import figure, subplot, imread, ceil, zeros, pi, e, arange, \
+meshgrid, cm, imshow, show
 from mpl_toolkits.mplot3d import Axes3D
 
+# Return value of Gauss on given x and y with scale s and size of n (= 6 * s)
 def f(s, n, x, y):
    """Gaussian function"""
    return 1 / (2 * pi * (s ** 2)) * (e ** -(((x - n / 2)**2 + (y - n / 2)**2 ) 
@@ -10,59 +12,48 @@ def f(s, n, x, y):
         
 # Gaussian filter with a kernel of 6s - 1 values
 def gauss(s):
-    """Two dimensional Gaussian mask"""
-    # 1 for verbose, 0 for non-verbose
-    debug = 0;    
-    
-    # if s is even, the heighest value will not be in the middle of the 2D
-    # array.
-    # a = 1 for even and 0 for odd
-    #a = 1 if (int(s) % 2 == 0) else 0
-        
+    """Contruct a 2-D Gaussian mask"""        
     # for sufficient result use ceil(6s) by ceil(6s) for a gaussian filter
     # read: http://en.wikipedia.org/wiki/Gaussian_blur for more explaination     
     n = int(ceil(6 * s) + 1)
     s = float(s)
     
-    # n * n zero matrix of floats3
+    # n * n zero matrix of floats
     gaussFilter = zeros((n, n), dtype=float)
 
-    for i in range(n):
-        for j in range(n):
-            gaussFilter[i][j] = f(s, n, i, j)
+    # fill gaussFilter[][] with gaussian values on x and y
+    for x in range(n):
+        for y in range(n):
+            gaussFilter[x][y] = f(s, n, x, y)
             
-            if debug: print("%.4f" % gaussFilter[i][j]),
-        if debug: print '\n'
-    
     # All values between 0 and 1
-    return gaussFilter #/ gaussFilter.sum()
+    return gaussFilter / gaussFilter.sum()
     
-F = imread('cameraman.png') 
+Image = imread('cameraman.png') 
 
-B = gauss(3)
+Gs = gauss(3)
 
-figure(1)
 fig = figure()
 
-X = arange(0, B.shape[0])
-Y = arange(0, B.shape[1])
+X = arange(0, Gs.shape[0])
+Y = arange(0, Gs.shape[1])
 
+# create meshgrid
 X, Y = meshgrid(X, Y)
 
+# matplot lib version < 1.0 uses Axes3D
 ax = Axes3D(fig)
 
-surf = ax.plot_surface(X, Y, B, rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=False)
+surf = ax.plot_surface(X, Y, Gs, rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=False)
 
 figure(2)
-subplot(121)
-imshow(B, cmap='gray')
-G = convolve(F, B, mode='nearest')
-subplot(122)
+# Convolution with gauss function (B)
+G = convolve(Image, Gs, mode='nearest')
+subplot(131)
+# or
+imshow(Image, cmap ='gray')
+subplot(132)
+imshow(Gs, cmap='gray')
+subplot(133)
 imshow(G, cmap ='gray') 
 show()
-#subplot(111)
-#imshow(F, cmap ='gray')
-
-#subplot(122)
-
-#show()
