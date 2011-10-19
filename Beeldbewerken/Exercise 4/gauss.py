dea0e8af880ba93c5d4e74d9348bccbf8f2bc872
Jayke Meijer <jayke.meijer@gmail.com>
2011-10-19 15:36:36 +0200

    Added gauss1 function. Needs debugging.

--- Beeldbewerken/Exercise 4/gauss.py
+++ Beeldbewerken/Exercise 4/gauss.py
@@ -1,5 +1,5 @@
 # By Jayke and Richard
-from scipy.ndimage import convolve
+from scipy.ndimage import convolve, convolve1d
 from pylab import figure, subplot, imread, ceil, zeros, pi, e, arange, \
 meshgrid, cm, imshow, show
 from mpl_toolkits.mplot3d import Axes3D
@@ -9,6 +9,11 @@ def f(s, n, x, y):
    """Gaussian function"""
    return 1 / (2 * pi * (s ** 2)) * (e ** -(((x - n / 2)**2 + (y - n / 2)**2 ) 
                                                               / (2 * (s ** 2))))
+                                                              
+# Return value of Gauss on given x and y with scale s and size of n (= 6 * s)
+def f1(s, n, x):
+   """Gaussian function"""
+   return 1 / (2 * pi * (s ** 2)) * (e ** -(((x - n / 2)**2) / (2 * (s ** 2))))
         
 # Gaussian filter with a kernel of 6s - 1 values
 def gauss(s):
@@ -29,9 +34,27 @@ def gauss(s):
     # All values between 0 and 1
     return gaussFilter / gaussFilter.sum()
     
-Image = imread('cameraman.png') 
+def gauss1(s):
+    '''Construct a 1-D Gaussian mask''' 
+    # for sufficient result use ceil(6s) by ceil(6s) for a gaussian filter
+    # read: http://en.wikipedia.org/wiki/Gaussian_blur for more explaination     
+    n = int(ceil(6 * s) + 1)
+    s = float(s)
+    
+    # n * n zero matrix of floats
+    gaussFilter = zeros((n), dtype=float)
+
+    # fill gaussFilter[] with gaussian values on x
+    for x in range(n):
+        gaussFilter[x] = f1(s, n, x)
+            
+    # All values between 0 and 1
+    return gaussFilter / gaussFilter.sum()
+   
+Image = imread('cameraman.png')
 
 Gs = gauss(3)
+Gsx = gauss1(3)
 
 fig = figure()
 
@@ -47,13 +70,29 @@ ax = Axes3D(fig)
 surf = ax.plot_surface(X, Y, Gs, rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=False)
 
 figure(2)
+
 # Convolution with gauss function (B)
 G = convolve(Image, Gs, mode='nearest')
-subplot(131)
-# or
+G2 = convolve1d(Image, Gsx, axis=0, mode='nearest')
+G2 = convolve1d(Image, Gsx, axis=1, mode='nearest')
+
+# Original image
+subplot(311)
 imshow(Image, cmap ='gray')
-subplot(132)
+
+# Mask 1
+subplot(323)
 imshow(Gs, cmap='gray')
-subplot(133)
+
+# Image with mask 1
+subplot(324)
 imshow(G, cmap ='gray') 
+
+# Mask 2 in one direction
+subplot(325)
+imshow((Gsx, Gsx), cmap='gray')
+
+# Image with mask 2
+subplot(326)
+imshow(G2, cmap ='gray') 
 show()
